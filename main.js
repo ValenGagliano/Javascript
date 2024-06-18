53b6a5ec4f99febf79db7390b942a279bb53a18f
Valentino <valen.gagliano2003@gmail.com>
2024-06-17 20:41:51 -0400

    cambios 3

--- main.js
+++ main.js
@@ -47,3 +47,52 @@ if(CorreoDelUsuario.includes("@gmail.com")){
 }
 
 
+alert ( "Gracias por Registrate " + NombreDelUsuraio + " En Esta Pagina Contamos con Las Nike AirJordan, Adidas Retro y Puma modelo 180 En stock")
+
+
+let salir 
+
+while (salir !== "salir") {
+
+    const ingresaZapatillas = prompt ( "Caul de las zapatillas que tenemos en stock te gustaria llevar? ")
+
+
+console.log(ingresaZapatillas)
+
+ if (ingresaZapatillas === "Nike AirJordan") {
+
+alert ("Perfecto El Final Seria De 120.000")
+
+}
+
+
+if ( ingresaZapatillas === "Adidas Retro") {
+
+
+    alert ("Muchas Gracias El precio Seria De 145.000")
+
+}
+if ( ingresaZapatillas === "Puma modelo 180") {
+
+
+
+    alert ("Muchas Gracias El Precio Seria de 110.000")
+}
+
+
+
+const final = prompt ("Te gustaria Agregar Algo Mas ?")
+
+if ( final === "salir"){
+
+salir  = final
+
+}
+
+
+
+}
+
+
+
+
