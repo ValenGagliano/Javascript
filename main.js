e11a244ab51354738e2d1118f14c375c98deed98
Valentino <valen.gagliano2003@gmail.com>
2024-06-22 23:42:33 -0400

    cambios finales

--- main.js
+++ main.js
@@ -1,97 +1,127 @@
 
-let NombreDelUsuraio = prompt ("Ingrese Su Nombre De Usuario") 
-
-if (NombreDelUsuraio.length > 1 ) {
 
 
-    alert ( "Gracias " + NombreDelUsuraio + " Por registrar tu usuario");
+let nombre = prompt ("Ingrese Su nombre");
+
+if (nombre.length > 1){ {
 
 
-}
-
-else {
-
-
-    alert( "Usuario No Registrado");
-}
-
-
-
-
-
-let EdadDelUsuario = prompt ("Ingrese Su Edad")
-
-
-
-if (EdadDelUsuario >= "18") {
-
-
-    alert ("Gracias Por Ingresar La Edad Del Usuario");
+alert ("Bienvenido " + nombre + " a la mejor tienda de zapatillas")
 
 }
 
-else {
+    function PedirEdad (){
 
-
-    alert ("Usted Debe Ser Mayor De Edad Para Continuar")
-}
-
-
-
-let CorreoDelUsuario = prompt("Ingresa Su Correo Electronico")
-
-if(CorreoDelUsuario.includes("@gmail.com")){
-    alert ("Su Correo Fue Verificado")
-}else{
-    alert ("Su Correo Es Invalido")
-}
-
-
-alert ( "Gracias por Registrate " + NombreDelUsuraio + " En Esta Pagina Contamos con Las Nike AirJordan, Adidas Retro y Puma modelo 180 En stock")
-
-
-let salir 
-
-while (salir !== "salir") {
-
-    const ingresaZapatillas = prompt ( "Caul de las zapatillas que tenemos en stock te gustaria llevar? ")
-
-
-console.log(ingresaZapatillas)
-
- if (ingresaZapatillas === "Nike AirJordan") {
-
-alert ("Perfecto El Final Seria De 120.000")
-
-}
-
-
-if ( ingresaZapatillas === "Adidas Retro") {
-
-
-    alert ("Muchas Gracias El precio Seria De 145.000")
-
-}
-if ( ingresaZapatillas === "Puma modelo 180") {
+        let edad = prompt ("Usted debre ingresar su edad")
+    
+    
+    if (edad >= 18) {
+    
+    alert ("Gracias por verificar que su edad es de " + edad + " ."  )
+    
+    } 
+    
+    else if (edad < 18 ) { {
+    
+        alert ( "Ustede debe Ser mayor de edad para continuar.")
+    }}}
+    
+    PedirEdad ();
+    
+    function PedirCorreo (){
+    
+    let CorreoElectronico = prompt (" Para finalizar el registro de usuario le pediremos su correo electronico.")
+    
+    if (CorreoElectronico.includes ("@gmail.com"))
+    
+        alert ("Tu correo " + CorreoElectronico + " fue registrado con exito.")
+    
+    else if (CorreoElectronico.length < 1) {{
+    
+    alert ("Su correo no es valido, intente nuevamente")
+    }}}
+    
+    PedirCorreo ();
 
 
 
-    alert ("Muchas Gracias El Precio Seria de 110.000")
-}
+    function elegirZapatillas() {
+        let opciones = prompt("A dia de hoy contamos con 3 zapatillas en stock n\ 1. Adidas Retro n\ 2. Nike AirJordan n\ 3. Puma Clasic 180 ")
+        const precioAdidas = 123000;
+    
+        const iva = (a,b) => a * b
+    
+        const resultado = iva(precioAdidas, 0.21)
+    
+        const precioFinal = (a,b) => a + b
+    
+        const resultadoFinalAdidas = precioFinal(precioAdidas, resultado)
+    
+        if (opciones == 1 || opciones == " Adidas Retro") {
+    
+            alert("Su Precio final es de " + resultadoFinalAdidas + " Muchas Gracias por su compra.")
+    
+        }
+    
+    
+        else if (opciones == 2 || opciones == "Nike AirJordan"){{ 
+            const precioNike = 145000;
+    
+            const iva = (a,b) => a * b
+        
+            const resultado = iva(precioNike, 0.21)
+        
+            const precioFinal = (a,b) => a + b
+        
+            const resultadoFinalNike = precioFinal(precioNike, resultado)
+            
+            
+            alert ("Su precio final es de " + resultadoFinalNike + " Muchas gracias por su compra.")
+
+        }}
+
+
+        else if (opciones == 3 || opciones == "Puma Clasic 180"){
+
+            const precioPuma = 110000;
+    
+            const iva = (a,b) => a * b
+        
+            const resultado = iva(precioPuma, 0.21)
+        
+            const precioFinal = (a,b) => a + b
+        
+            const resultadoFinalPuma = precioFinal(precioPuma, resultado)
+            
+            
+            alert ("Su precio final es de " + resultadoFinalPuma + " Muchas gracias por su compra.")
+
+
+        }}
+    
+    elegirZapatillas ();
+
+} 
 
 
 
-const final = prompt ("Te gustaria Agregar Algo Mas ?")
-
-if ( final == "salir"){
-
-salir = final
-
-}
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
